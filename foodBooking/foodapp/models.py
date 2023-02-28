145d467ab0945a1e8d0de3a476e141c78448bbd7
Raya513 <64854191+Raya513@users.noreply.github.com>
2023-02-28 09:33:38 +0530

    Update models.py

--- foodBooking/foodapp/models.py
+++ foodBooking/foodapp/models.py
@@ -1,21 +1,29 @@
-
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=20)
 
-class Menu(models.Model):
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    customer_name = models.CharField(max_length=200)
-    customer_address = models.CharField(max_length=200)
-    customer_phone_number = models.CharField(max_length=20)
-    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    order_time = models.DateTimeField(auto_now_add=True)
+    items = models.ManyToManyField(MenuItem)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
